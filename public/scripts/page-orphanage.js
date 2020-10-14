54a697133e4026d684dea5f0d57f204960707049
StephanieGadoni <developerbyste@gmail.com>
2020-10-14 12:52:23 -0300

    add paginas novas e corrigir o JS

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,44 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false,
+}
+
+const map = L.map('mapid', options).setView([-27.2109325,-49.6448719], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popAnchor: [170, 2]
+})
+
+L
+.marker([-27.2109325,-49.6448719], { icon })
+.addTo(map)
+
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove('active');
+    }
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector('.orphanage-details > img')
+
+    imageContainer.src = image.src
+
+    button.classList.add('active')
+}
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,27 +1,23 @@
-//Create map
-const map = L.map("mapid").setView([-16.0085155,-48.0709786], 14); //lat, long, zoom
+const map = L.map('mapid').setView([-27.2109325,-49.6448719], 15);
 
-//Create and add tileLayer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
 
-//Create Icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg", //icone
-    iconSize: [58, 68],                         //tamanho [larg Alt]
-    iconAnchor: [29, 68], //onde ficará o icone                     
-    popupAnchor: [170,2] // onde ficará o popup
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170, 2]
 })
-
-//Create popup overlay
 
 const popup = L.popup({
     closeButton: false,
-    className: 'map-popup',
+    className: "map-popup",
     minWidth: 240,
-    minHeigth: 240
+    minHeight: 240
 }).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
 
-//Create and marker
-L.marker([-16.0085155,-48.0709786], { icon })
+L
+.marker([-27.2109325,-49.6448719], { icon })
 .addTo(map)
 .bindPopup(popup)
